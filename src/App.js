3b8467554ca417873a14156d1ca3acdf83600637
alan-ramirez <ramirezalanm@gmail.com>
2022-03-21 23:48:56 -0300

    segunda entrega

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import logo1 from './logo1.png';
+import logo from '../src/logo.svg';
 import './App.css';
 import Navbar1 from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -9,41 +8,9 @@ import * as ReactBootStrap from 'react-bootstrap';
 function App() {
   return (
     <div className="App">
-      <ReactBootStrap.Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <ReactBootStrap.Container>
-        <ReactBootStrap.Navbar.Brand href="#home">
-          <img
-            src={logo1}
-            width="100"
-            height="70"
-            className="d-inline-block align-top"
-            alt="logo"    
-          />
-        </ReactBootStrap.Navbar.Brand>
-        <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
-          <ReactBootStrap.Nav className="me-auto">
-            <ReactBootStrap.Nav.Link href="#features">Inicio</ReactBootStrap.Nav.Link>
-            <ReactBootStrap.Nav.Link href="#pricing">Nosotros</ReactBootStrap.Nav.Link>
-            <ReactBootStrap.NavDropdown title="Productos" id="collasible-nav-dropdown">
-              <ReactBootStrap.NavDropdown.Item href="#action/3.1">Guitarras</ReactBootStrap.NavDropdown.Item>
-              <ReactBootStrap.NavDropdown.Item href="#action/3.2">Percusión</ReactBootStrap.NavDropdown.Item>
-              <ReactBootStrap.NavDropdown.Item href="#action/3.3">Teclados</ReactBootStrap.NavDropdown.Item>
-              <ReactBootStrap.NavDropdown.Divider />
-              <ReactBootStrap.NavDropdown.Item href="#action/3.4">Equipamiento</ReactBootStrap.NavDropdown.Item>
-            </ReactBootStrap.NavDropdown>
-          </ReactBootStrap.Nav>
-          <ReactBootStrap.Nav>
-            <ReactBootStrap.Nav.Link href="#deets">Login</ReactBootStrap.Nav.Link>
-            <ReactBootStrap.Nav.Link eventKey={2} href="#memes">
-              Carrito
-            </ReactBootStrap.Nav.Link>
-          </ReactBootStrap.Nav>
-        </ReactBootStrap.Navbar.Collapse>
-        </ReactBootStrap.Container>
-      </ReactBootStrap.Navbar>
+    <Navbar1 />
       <header className="App-header">
-        <Navbar1 />
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,47 @@
 import React from 'react';
+import logo1 from '../logo1.png';
+import '../App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import {Navbar} from 'react-bootstrap';
+import * as ReactBootStrap from 'react-bootstrap';
 
-const Navbar1 = () => {
-    return <div>Hola</div>;
-};
+function Navbar1() {
+    return (
+      <div className="App">
+        <ReactBootStrap.Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <ReactBootStrap.Container>
+          <ReactBootStrap.Navbar.Brand href="#home">
+            <img
+              src={logo1}
+              width="100"
+              height="70"
+              className="d-inline-block align-top"
+              alt="logo"    
+            />
+          </ReactBootStrap.Navbar.Brand>
+          <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
+            <ReactBootStrap.Nav className="me-auto">
+              <ReactBootStrap.Nav.Link href="#features">Inicio</ReactBootStrap.Nav.Link>
+              <ReactBootStrap.Nav.Link href="#pricing">Nosotros</ReactBootStrap.Nav.Link>
+              <ReactBootStrap.NavDropdown title="Productos" id="collasible-nav-dropdown">
+                <ReactBootStrap.NavDropdown.Item href="#action/3.1">Guitarras</ReactBootStrap.NavDropdown.Item>
+                <ReactBootStrap.NavDropdown.Item href="#action/3.2">Percusión</ReactBootStrap.NavDropdown.Item>
+                <ReactBootStrap.NavDropdown.Item href="#action/3.3">Teclados</ReactBootStrap.NavDropdown.Item>
+                <ReactBootStrap.NavDropdown.Divider />
+                <ReactBootStrap.NavDropdown.Item href="#action/3.4">Equipamiento</ReactBootStrap.NavDropdown.Item>
+              </ReactBootStrap.NavDropdown>
+            </ReactBootStrap.Nav>
+            <ReactBootStrap.Nav>
+              <ReactBootStrap.Nav.Link href="#deets">Login</ReactBootStrap.Nav.Link>
+              <ReactBootStrap.Nav.Link eventKey={2} href="#memes">
+                Carrito
+              </ReactBootStrap.Nav.Link>
+            </ReactBootStrap.Nav>
+          </ReactBootStrap.Navbar.Collapse>
+          </ReactBootStrap.Container>
+        </ReactBootStrap.Navbar>
+      </div>
+    );
+  }
 export default Navbar1;
