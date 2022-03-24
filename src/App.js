bfd8b149839cb7def72ccde64bcdf54f2473d938
alan-ramirez <ramirezalanm@gmail.com>
2022-03-23 21:33:31 -0300

    segunda entrega desafio3

--- src/App.js
+++ src/App.js
@@ -6,12 +6,25 @@ import {Navbar} from 'react-bootstrap';
 import * as ReactBootStrap from 'react-bootstrap';
 import ItemList from './components/ItemListContainer';
 import ItemListContainer from './components/ItemListContainer';
+import React, { useState, useEffect} from 'react';
 
 function App() {
+  const [contar, setContar] = useState (0);
+
+  useEffect ( () => {
+    console.log("Total: " + contar)
+  }, [contar]);
+
+  const encenderApagar = () => {
+    setContar (contar + 1)
+  };
+
   return (
     <div className="App">
     <Navbar1 />
     <ItemListContainer />
+
+
       <header className="App-header">
 
         <img src={logo} className="App-logo" alt="logo" />
@@ -26,6 +39,9 @@ function App() {
         >
           Learn React
         </a>
+        <h4>Clicks: {contar} </h4>
+        <button onClick={encenderApagar}>Encender / Apagar</button>
+
       </header>
     </div>
   );
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -2,13 +2,30 @@ import React from 'react';
 import '../App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import * as ReactBootStrap from 'react-bootstrap';
+import ReactDOM from 'react-dom'
 
-function ItemListContainer() {
-    return (
-        <ReactBootStrap.Nav.Link eventKey={2} href="#memes">
-        Texto provisional
-      </ReactBootStrap.Nav.Link>
 
-    );
+function UserGreeting(props) {
+    return <h1>Bienvenido!</h1>;
   }
+  
+function GuestGreeting(props) {
+    return <h1>Inicia sesión</h1>;
+  }
+
+
+function ItemListContainer(props) {
+    const isLoggedIn = props.isLoggedIn;
+    if (isLoggedIn) {
+        return <UserGreeting />;
+      }
+      return <GuestGreeting />;
+}
+
+ReactDOM.render(
+    // fijado isLoggedIn={true}:
+    <ItemListContainer isLoggedIn={true} />,
+    document.getElementById('root')
+  );
+
 export default ItemListContainer;
